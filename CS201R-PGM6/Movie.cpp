a84165d4c43a21bad58799c47cc0efad1406ba95
adeeba2004 <157551043+adeeba2004@users.noreply.github.com>
2024-11-17 01:28:37 -0600

    Delete CS201R-PGM6 directory

--- CS201R-PGM6/Movie.cpp
+++
@@ -1,8 +0,0 @@
-#include "Movie.h"
-
-Movie::Movie(const std::string& title, const std::string& director, int releaseYear)
-    : Media(title, releaseYear), director(director) {}
-
-std::string Movie::getDirector() const {
-    return director;  // Implement this function
-}
